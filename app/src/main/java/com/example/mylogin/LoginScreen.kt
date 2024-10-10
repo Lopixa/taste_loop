221593c6136173dbca020509a35e6ae0b890e587
yudhis <yudhistirachairilaffa@gmail.com>
2024-10-10 14:10:36 +0800

    hj

--- app/src/main/java/com/example/mylogin/LoginScreen.kt
+++ app/src/main/java/com/example/mylogin/LoginScreen.kt
@@ -76,3 +76,52 @@ fun LoginScreen(){
         }
     }
 
+@Composable
+fun LoginScreen2(){
+
+    var email by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.download), contentDescription = "Login Image ",
+            modifier = Modifier.size(200.dp))
+
+        Text(text = "Welcome Back", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Text(text = "Login To Your Account ")
+
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = email, onValueChange = {
+            email = it
+        }, label = {
+            Text(text = "Email Address")
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        }, label = {
+            Text(text = "Password")
+        }, visualTransformation = PasswordVisualTransformation())
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            Log.i("Credential", "Email : $email Password : $password")
+        }) {
+            Text(text = "Login")
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedButton(onClick = {  }) {
+            Text(text = "Forgot Password?")
+        }
+    }
+}
